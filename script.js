f95ff960e4ec21be59f31c03121b2624dcd4f261
jambrizz <ambrizjovani@gmail.com>
2021-05-05 01:34:18 -0700

    Initial Commit

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function lastM() {
     let dateT = document.lastModified;
     document.getElementById("date").innerHTML = dateT;
   }
+  /* $(selector) .ready(lastM(){
+    alert("Hello World!");
+    $("#blackbox") .hide();
+  }); */
