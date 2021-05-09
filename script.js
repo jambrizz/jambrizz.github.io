840bb5dd1dc59737d47474f8511f751efb63dfa4
jambrizz <ambrizjovani@gmail.com>
2021-05-08 21:09:41 -0700

    Initial Commit

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function lastM() {
-    let dateT = document.lastModified;
-    document.getElementById("date").innerHTML = dateT;
-  }
-
-/*function year() {
-  let d = new Date();
-  let year = d.getFullYear();
-  document.getElementById("currentYear").innerHTML = year;
-}*/
 $(document).ready(function(){
   $('.currentYear').text( new Date().getFullYear() );
 });
