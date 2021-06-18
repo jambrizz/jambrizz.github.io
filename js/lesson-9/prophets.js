22e65521ea31c51dba19e0ed2548147a559889b0
jambrizz <ambrizjovani@gmail.com>
2021-06-18 01:53:57 -0700

    update

--- js/lesson-9/prophets.js
+++ js/lesson-9/prophets.js
@@ -5,4 +5,12 @@ fetch(requestURL)
     })
     .then(function (jsonObject) {
         console.table(jsonObject); //temporary checking for valid respons and parsing
+    const prophets = jsonObject['prophets'];
+     for (let i = 0; i < prophets.length; i++) {
+      let card = document.createElement('section');
+       let h2 = document.createElement('h2');
+       h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+       card.appendChild(h2);
+       document.querySelector('div.cards').appendChild(card);
+    }
     });
